d74a31c07e18c8da95fbe0a2773034b2b19e37da
Yongjae <k33721@gmail.com>
2020-04-07 14:27:48 +0900

    Icons and Styling

--- App.js
+++ App.js
@@ -36,7 +36,6 @@ export default class extends React.Component {
         coords:{ latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
     } catch (error){
       Alert.alert("Can't find you.", "So sad");
     }
@@ -47,8 +46,12 @@ export default class extends React.Component {
     this.getLocation();
   };
   render() {
-    const{ isLoading, temp , condition} = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
+    const{ isLoading, temp , condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   };
 }
 
